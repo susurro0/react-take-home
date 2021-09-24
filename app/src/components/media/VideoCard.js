847c369aa6f934dcbf6c7b3cc6cc06f04618c9fc
susurro0 <us.cingoez@gmail.com>
2021-09-24 15:12:37 -0400

    create Video Card (no style)

--- app/src/components/media/VideoCard.js
+++ app/src/components/media/VideoCard.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./VideoCard.css";
+import { ButtonGroup, Spinner, Card, Button, Row, Col } from "react-bootstrap";
+import Video from "./Video";
+import Image from "./MediaImage";
 
+import { BsDownload, BsLink } from "react-icons/bs";
+
+function simulateNetworkRequest() {
+  return new Promise((resolve) => setTimeout(resolve, 2000));
+}
 function VideoCard(props) {
-  
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const handleClick = (e) => {
+    setLoading(true);
+  };
+  const copyLinkHandler = () => {
+    console.log("Copy link");
+  };
   return (
-    <></>
+    <Card className="video_card">
+      <Image src="https://placekitten.com/200/300" />
+      <Row className="btn_container">
+        <ButtonGroup aria-label="Basic example">
+          <Button
+            variant="outline-secondary"
+            className="btn_copy"
+            size="lg"
+            onClick={copyLinkHandler}
+          >
+            <BsLink />
+          </Button>{" "}
+          <Button
+            variant="outline-secondary"
+            size="lg"
+            disabled={isLoading}
+            onClick={!isLoading ? handleClick : null}
+          >
+            {isLoading ? (
+              <Spinner animation="border" role="status" size="sm">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            ) : (
+              <BsDownload />
+            )}
+          </Button>
+        </ButtonGroup>
+      </Row>
+    </Card>
   );
 }
 
--- app/src/pages/HomeScreen.js
+++ app/src/pages/HomeScreen.js
@@ -3,11 +3,12 @@ import Campaign from '../components/campaign/Campaign';
 
 import MediaImage from '../components/media/MediaImage'
 import Video from '../components/media/Video';
+import VideoCard from '../components/media/VideoCard';
 function HomeScreen(props) {
   return (
     <div>
       
-      <Video src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"/>
+      <VideoCard src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"/>
     </div>
   );
 }
